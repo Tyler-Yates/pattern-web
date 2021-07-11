59faed8ca821f8edffa8e787a362b8d95f36baea
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-07-10 22:38:11 -0500

    Use different collection

--- application/data/document_dao.py
+++ application/data/document_dao.py
@@ -10,6 +10,9 @@ from pymongo.results import InsertOneResult
 # Set the documents to expire after a set amount of time
 TTL_SECONDS = 6 * 60 * 60  # expire after 6 hours
 
+# Collections
+PATTERN_COLLECTION_NAME = "pattern"
+
 # Fields used in the database
 CREATED_AT_FIELD = "createdAt"
 TEXT_FIELD = "text"
@@ -29,24 +32,21 @@ class DocumentDao:
 
         # Set up database and collection variables
         self.database = database
-        self.test_collection: Collection = database.test_collection
-
-        # Set a TTL on the test collection
-        self.test_collection.create_index(CREATED_AT_FIELD, expireAfterSeconds=TTL_SECONDS)
+        self.pattern_collection: Collection = database.get_collection(PATTERN_COLLECTION_NAME)
 
         print(f"Database collections: {self.database.list_collection_names()}")
 
     def insert_document(self, text: str) -> InsertOneResult:
-        return self.test_collection.insert_one({CREATED_AT_FIELD: datetime.datetime.utcnow(), TEXT_FIELD: text})
+        return self.pattern_collection.insert_one({CREATED_AT_FIELD: datetime.datetime.utcnow(), TEXT_FIELD: text})
 
     def get_documents(self):
-        return self.test_collection.find()
+        return self.pattern_collection.find()
 
     def get_document(self, document_id) -> dict:
-        return self.test_collection.find_one({ID_FIELD: ObjectId(document_id)})
+        return self.pattern_collection.find_one({ID_FIELD: ObjectId(document_id)})
 
     def delete_document(self, document_id):
-        return self.test_collection.delete_one({ID_FIELD: ObjectId(document_id)})
+        return self.pattern_collection.delete_one({ID_FIELD: ObjectId(document_id)})
 
     def delete_documents(self):
-        return self.test_collection.delete_many({})
+        return self.pattern_collection.delete_many({})
