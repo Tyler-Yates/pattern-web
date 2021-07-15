7fecb989bc6a8ad7572118af3da230fa774697f6
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-07-14 22:09:42 -0500

    Kafka work

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,17 @@
 import logging
+import os
 
 from flask import Flask, request, redirect
 
 from application.data.custom_json_encoder import CustomJsonEncoder
 from application.data.document_dao import DocumentDao
+from application.data.kafka_dao import KafkaDao
 from application.routes.api_routes import API_BLUEPRINT
 from application.routes.html_routes import HTML_BLUEPRINT
 
 # Key to use when accessing flask app config
 DATABASE_CONFIG_KEY = "DB"
+KAFKA_CONFIG_KEY = "KF"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -30,8 +33,16 @@ def create_flask_app() -> Flask:
     # Set custom JSON encoder to handle MongoDB ObjectID
     app.json_encoder = CustomJsonEncoder
 
-    # Create a DAO and add it to the flask app config for access by the blueprints
+    # Create the database DAO and add it to the flask app config for access by the blueprints
     app.config[DATABASE_CONFIG_KEY] = DocumentDao()
+
+    # Create the Kafka DAO and add it to the flask app config for access by the blueprints
+    app.config[KAFKA_CONFIG_KEY] = KafkaDao(
+        os.environ["CLOUDKARAFKA_BROKERS"],
+        os.environ["CLOUDKARAFKA_USERNAME"],
+        os.environ["CLOUDKARAFKA_PASSWORD"],
+        os.environ["ENCRYPTION_KEY"]
+    )
 
     # Set up the app
     _setup_app(app)
